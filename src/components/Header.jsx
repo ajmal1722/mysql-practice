9c4ce04e67c76013de84cea92e7fe741b5116e6d
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-11-01 17:31:31 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='h-20 flex items-center bg-red-500'>
+            Header
+        </div>
+    )
+}
+
+export default Header
