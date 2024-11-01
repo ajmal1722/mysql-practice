78d15b24fdcfe0d95e4ee51620579f6a1a811acc
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-11-01 17:45:49 +0530

    rearranged file structure

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Header = () => {
-    return (
-        <div className='h-20 flex items-center bg-red-500'>
+  return (
+        <div className='h-20 flex items-center bg-red-500 px-5'>
             Header
         </div>
-    )
+  )
 }
 
 export default Header
